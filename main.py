fd8ede3383dc58347310ca1cdfaff9e955941277
kourou <82067647+1259334154@users.noreply.github.com>
2022-11-09 14:16:20 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from tab3 import page_3
 from tab1 import page_1
 from tab2 import page_2
 from tab4 import page_4
-from image import images
 
 
 st.set_page_config(page_icon="🌴", page_title="###", layout="wide")
@@ -14,7 +13,6 @@ st.title('##'*10)
 
 tab1, tab2, tab3, tab4 = st.tabs(["page_1", "page_2", "page_3",'pag_4'])
 with tab1:
-    images()
     page_1()
 with tab3:
     page_3()
