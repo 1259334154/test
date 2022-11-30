87d2a55c89bd72418c2e3f9868f128e8f8c91993
kourou <82067647+1259334154@users.noreply.github.com>
2022-11-30 20:30:13 +0800

    Add files via upload

---
+++ tab1.py
@@ -0,0 +1,21 @@
+import streamlit as st
+def page_1():
+    st.markdown(" ---")
+    st.header("Introduction of adrenal glands Atlas")
+    st.markdown(f'<p style="font-size:24px;border-radius:2%;">{"Human adrenal glands are essential endocrine organs, the cortex produces steroid hormones, while the inner medulla contains neural crest (ectoderm) cell secrete catecholamines. The cortex including zona glomerulosa (zG) (mineralocorticoids), zona fasciculata (zF) (glucocorticoids) and zona reticularis (zR) (androgen precursors). "}</p >', unsafe_allow_html=True)
+    st.markdown(f'<p style="font-size:24px;border-radius:2%;">{"Human fetal adrenal glands are very different from those of adults. Composed of the definitive zone (DZ) and fetal zone (FZ) and the transition zone (TZ) from approximately 14 GW, the organ showed no medulla in the fetus."}</p >', unsafe_allow_html=True)
+    st.markdown(f'<p style="font-size:24px;border-radius:2%;">{"Tree shrew is known to be conserved member of the primate. It had been proved that Tree shrew adrenal glands divided into a medulla and a cortex (ZG, ZF and ZR) and DHEA could be synthesized. "}</p >', unsafe_allow_html=True)
+    st.markdown(f'<p style="font-size:24px;border-radius:2%;">{"Libraries were prepared as outlined by the 10x Genomics Single Cell 3′v2 Reagent Kit user guide. 14,481 high‐quality cells from 3 patients adrenal glands passed quality control and grouped into 20 major clusters. 75,482 adrenal cells from 12 fetals (7-14 gestational weeks) and cluster 12 main groups. A total of 47096 cell transcriptomes from the eight tree shrews were retained and subgroup into 13 major clusters."}</p >', unsafe_allow_html=True)
+
+    st.markdown(" ---")
+    st.header("original article links")
+    st.markdown(f'<p style="font-size:24px;border-radius:2%;">{"1.Single‐cell transcriptomes reveal characteristic features of cell types within the human adrenal microenvironment.  https://onlinelibrary.wiley.com/doi/10.1002/jcp.30398"}</p >', unsafe_allow_html=True)
+    st.markdown(f'<p style="font-size:24px;border-radius:2%;">{"2.A spatiotemporal steroidogenic regulatory network in human fetal adrenal glands and gonads. https://www.frontiersin.org/articles/10.3389/fendo.2022.1036517/full"}</p >', unsafe_allow_html=True)
+
+
+    st.markdown(" ---")
+    st.subheader("Contact us")
+    st.caption("Center for Genomic and Personalized Medicine, Guangxi Medical University, Nanning, Guangxi 530021, China.")
+    st.caption("Phone: 86-0771-5641040")
+    st.caption("Email: mozengnan@gxmu.edu.cn; jiangyonghua@stu.gxmu.edu.cn")
+
