1cf5e5758041db2a247644e0f1edf5e892861af4
kourou <82067647+1259334154@users.noreply.github.com>
2023-03-13 20:48:46 +0800

    Update tab4.py

--- tab4.py
+++ tab4.py
@@ -12,7 +12,7 @@ def page_4():
     # file = 'G:/cacase-random forest.csv'
 
     # if file is not None:
-    df = pd.read_csv('./Tree shrew adrenal.csv', encoding="gbk")
+    df = pd.read_csv('./all.csv', encoding="gbk")
 
     def draw_table(df, height, width):  # css画表
         columns = df.columns
